7a8dc728ec3b3c17ef4d716f9b6d8d9db13bbcb4
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-19 17:46:55 -0500

    ✨ feat: Get and show characters from Rick and Morty API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+ import { useState, useEffect } from 'react';
+import { Card } from './components/Card';
+import { API_URL } from './utils/constants';
 import './App.css'
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
+        return response.json()
+      })
+      .then(data => setData(data))
+  }, []);
 
   return (
-    <>
-    </>
+    <main className='container'>
+      <h1 className='title'>Rick and Morty Wiki</h1>
+        {data && (
+          <section className='card-container'>
+            {
+              data.results.map((character) => {
+                return (
+                  <Card key={character.id} character={character} />
+                  )
+              })
+            }
+          </section>
+        )}
+    </main>
   )
 }
 
