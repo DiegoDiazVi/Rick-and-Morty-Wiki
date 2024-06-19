a060f2311b1037c26b2384e7fcdec25d1c423bbf
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-19 18:48:19 -0500

    ✨ feat: Add search functionality to the Rick and Morty Wiki app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
- import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Card } from './components/Card';
 import { API_URL } from './utils/constants';
 import './App.css'
+import { SearchBar } from './components/SearchBar';
 
 function App() {
   const [data, setData] = useState(null);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     fetch(API_URL)
@@ -17,9 +19,27 @@ function App() {
       .then(data => setData(data))
   }, []);
 
+  const handleInput = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const fetchCharacter = () => {
+    fetch(`${API_URL}?name=${search}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
+        return response.json();
+      })
+      .then(data => setData(data))
+      .catch(error => console.error(error))
+  }
+
+
   return (
     <main className='container'>
       <h1 className='title'>Rick and Morty Wiki</h1>
+      <SearchBar fetchCharacter={fetchCharacter} handleInput={handleInput} />
         {data && (
           <section className='card-container'>
             {
