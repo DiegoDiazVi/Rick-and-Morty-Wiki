426d77f8f035d1e640cf9d8959a2dd14cdf35a3e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-19 19:12:34 -0500

    ✨ feat: Update search button click event in SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ export const SearchBar = ({ fetchCharacter ,handleInput }) => {
     return (
         <section className='search-container'>
             <input className='search-input 'type="text" placeholder='Search for character' onBlur={handleInput}/>
-            <button className='search-button' onClick={fetchCharacter}>🔍</button>
+            <button className='search-button' onClick={() =>fetchCharacter(false)}>🔍</button>
         </section>
     )
 }
